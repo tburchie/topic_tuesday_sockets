380cbd2bcb763894b05de10a88829b26ab5125a0
tburchie <103402429+tburchie@users.noreply.github.com>
2024-12-17 17:18:23 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,51 +1,144 @@
 #include <iostream>
+#include <string>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 8080
-#define BUFFER_SIZE 1024
+// For Windows, include Windows specific headers
+#ifdef _WIN32
+#include <winsock2.h>
+#include <iphlpapi.h>
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#endif
+
+// Function to detect OS
+std::string get_os_name() {
+#ifdef _WIN32
+    return "Windows";
+#elif __linux__
+    return "Linux";
+#elif __APPLE__
+    return "macOS";
+#else
+    return "Unknown OS";
+#endif
+}
+
+// Function to get local IP address for Linux/macOS
+std::string get_local_ip() {
+    struct sockaddr_in sa;
+    char buffer[128];
+    
+    // Get the local IP address using a socket (for Linux/macOS)
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        return "";
+    }
+    
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(8080);  // Port doesn't matter, just need a valid IP
+    sa.sin_addr.s_addr = inet_addr("8.8.8.8");  // Google DNS (arbitrary external IP)
+    
+    // Try to connect to the remote address to resolve the local IP
+    if (connect(sockfd, (struct sockaddr*)&sa, sizeof(sa)) == -1) {
+        perror("Failed to connect");
+        close(sockfd);
+        return "";
+    }
+    
+    // Retrieve the local IP address used for the connection
+    socklen_t len = sizeof(sa);
+    if (getsockname(sockfd, (struct sockaddr*)&sa, &len) == -1) {
+        perror("Failed to get socket name");
+        close(sockfd);
+        return "";
+    }
+    
+    // Convert the IP to string and return it
+    inet_ntop(AF_INET, &(sa.sin_addr), buffer, sizeof(buffer));
+    close(sockfd);
+    
+    return std::string(buffer);
+}
+
+// Function to get local IP address for Windows
+std::string get_local_ip_windows() {
+    PIP_ADAPTER_INFO pAdapterInfo;
+    PIP_ADAPTER_INFO pAdapter = nullptr;
+    DWORD dwSize = sizeof(IP_ADAPTER_INFO);
+    char local_ip[16] = {0};
+    
+    pAdapterInfo = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
+    if (GetAdaptersInfo(pAdapterInfo, &dwSize) == NO_ERROR) {
+        pAdapter = pAdapterInfo;
+        while (pAdapter) {
+            if (pAdapter->IpAddressList.IpAddress.String[0] != '0') {
+                strcpy(local_ip, pAdapter->IpAddressList.IpAddress.String);
+                break;
+            }
+            pAdapter = pAdapter->Next;
+        }
+    }
+    
+    free(pAdapterInfo);
+    return std::string(local_ip);
+}
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[BUFFER_SIZE] = {0};
+    // Get OS and local IP
+    std::string os_name = get_os_name();
+    std::cout << "OS detected: " << os_name << std::endl;
 
+    std::string local_ip;
+#ifdef _WIN32
+    local_ip = get_local_ip_windows();
+#else
+    local_ip = get_local_ip();
+#endif
+
+    if (local_ip.empty()) {
+        std::cerr << "Error: Could not determine local IP address." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Local IP address: " << local_ip << std::endl;
+
+    // Set server address and port
+    std::string server_ip = "192.168.1.10"; // Replace with server's local IP
+    int port = 8080;
+    
     // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Socket creation failed");
+        return 1;
     }
 
-    // Set up server address structure
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IP address from text to binary format
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address");
-        exit(EXIT_FAILURE);
-    }
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
 
     // Connect to the server
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Connection failed");
-        exit(EXIT_FAILURE);
+        close(sock);
+        return 1;
     }
 
-    std::cout << "Connected to the server." << std::endl;
+    // Send message to the server
+    std::string message = "Hello from client!";
+    if (send(sock, message.c_str(), message.length(), 0) == -1) {
+        perror("Message send failed");
+        close(sock);
+        return 1;
+    }
+    
+    std::cout << "Message sent to server: " << message << std::endl;
 
-    // Receive message from the server
-    int valread = read(sock, buffer, BUFFER_SIZE);
-    std::cout << "Message from server: " << buffer << std::endl;
-
-    // Send a response to the server
-    const char* message = "Hello from client!";
-    send(sock, message, strlen(message), 0);
-
-    // Close the socket
+    // Close socket
     close(sock);
 
     return 0;
